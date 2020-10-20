2d85c3f367f5107cf950003fe44c6c12fbd98e46
Mitch Gilbert <mitchellpgilbert@gmail.com>
2020-10-20 00:41:56 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -236,6 +236,7 @@ def big_shoe_rebounds
         data.each do |data_item, element|
           if data_item[:shoe] == shoe_array.max()
             puts data_item[:player_name]
+          end 
         end
       end
     end
